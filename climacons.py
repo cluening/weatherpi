cac65a228d7b5069a7e7020a29cbc936a2f165af
Cory Lueninghoener <cluening@gmail.com>
2020-09-06 18:33:47 -0600

    Add climacons lookup table

---
+++ climacons.py
@@ -0,0 +1,203 @@
+climacons = {
+  # Clear
+  ## climacon sun
+  "sunny": "climacon sun",
+  "clear": "climacon sun",
+  "mostly_sunny": "climacon sun",
+  "mostly_clear": "climacon sun",
+  "passing_clounds": "climacon sun",
+  "more_sun_than_clouds": "climacon sun",
+  
+  ## climacon moon
+  "night_clear": "climacon moon",
+  "night_mostly_clear": "climacon moon",
+  
+  
+  # Clouds
+  ## climacon cloud sun
+  "scattered_clouds": "climacon cloud sun",
+  "partly_cloudy": "climacon cloud sun",
+  "a_mixture_of_sun_and_clouds": "climacon cloud sun",
+  "increasing_cloudiness": "climacon cloud sun",
+  "breaks_of_sun_late": "climacon cloud sun",
+  "afternoon_clouds": "climacon cloud sun",
+  "morning_clouds": "climacon cloud sun",
+  "partly_sunny": "climacon cloud sun",
+  "high_level_clouds": "climacon cloud sun",
+  "decreasing_cloudiness": "climacon cloud sun",
+  "clearing_skies": "climacon cloud sun",
+  "high_clouds": "climacon cloud sun",
+  "more_clouds_than_sun": "climacon cloud sun",
+  "broken_clouds": "climacon cloud sun",
+  "increasing_cloudiness": "climacon cloud sun",
+  "low_clouds": "climacon cloud sun",
+  
+  ## climacon cloud moon
+  "night_passing_clouds": "climacon cloud moon",
+  "night_scattered_clouds": "climacon cloud moon",
+  "night_partly_cloudy": "climacon cloud moon",
+  "night_afternoon_clouds": "climacon cloud moon",
+  "night_morning_clouds": "climacon cloud moon",
+  "night_decreasing_cloudiness": "climacon cloud moon",
+  "night_clearing_skies": "climacon cloud moon",
+  "night_broken_clouds": "climacon cloud moon",
+  "night_high_level_clouds": "climacon cloud moon",
+  "night_high_clouds": "climacon cloud moon",
+  
+  ## climacon cloud
+  "mostly_cloudy": "climacon cloud",
+  "cloudy": "climacon cloud",
+  "overcast": "climacon cloud",
+  "night_mostly_cloudy": "climacon cloud",
+  
+  
+  # Rain
+  ## climacon rain
+  "rain_early": "climacon rain",
+  "rain":  "climacon rain",
+  "rain_early": "climacon rain",
+  "rain_late": "climacon rain",
+  
+  ## climacon showers sun
+  "showers": "climacon showers sun",
+  "showery": "climacon showers sun",
+  "showers_early": "climacon showers sun",
+  "showers_late": "climacon showers sun",
+  "scattered_showers": "climacon showers sun",
+  "a_few_showers": "climacon showers sun",
+  "light_showers": "climacon showers sun",
+  "passing_showers": "climacon showers sun",
+  "rain_showers": "climacon showers sun",
+  "numerous_showers": "climacon showers sun",
+  "drizzle": "climacon showers sun",
+  "sprinkles": "climacon showers sun",
+  "light_rain": "climacon showers sun",
+  "sprinkles_early": "climacon showers sun",
+  "light_rain_early": "climacon showers sun",
+  "sprinkles_late": "climacon showers sun",
+  "light_rain_late": "climacon showers sun",
+  
+  ## climacon showers moon
+  "night_scattered_showers": "climacon showers moon",
+  "night_a_few_showers": "climacon showers moon",
+  "night_light_showers": "climacon showers moon",
+  "night_passing_showers": "climacon showers moon",
+  "night_rain_showers": "climacon showers moon",
+  "night_sprinkles": "climacon showers moon",
+  "night_showers": "climacon showers moon",
+  
+  ## climacon lightning
+  "strong_thunderstorms": "climacon lightning",
+  "severe_thunderstorms": "climacon lightning",
+  "thundershowers": "climacon lightning",
+  "thunderstorms": "climacon lightning",
+  "tstorms_early": "climacon lightning",
+  "isolated_tstorms_late": "climacon lightning",
+  "scattered_tstorms_late": "climacon lightning",
+  "tstorms_late": "climacon lightning",
+  "tstorms": "climacon lightning",
+  "widely_scattered_tstorms": "climacon lightning",
+  "isolated_tstorms": "climacon lightning",
+  "a_few_tstorms": "climacon lightning",
+  "scattered_tstorms": "climacon lightning",
+  "night_widely_scattered_tstorms": "climacon lightning",
+  "night_isolated_tstorms": "climacon lightning",
+  "night_a_few_tstorms": "climacon lightning",
+  "night_scattered_tstorms": "climacon lightning",
+  "night_tstorms": "climacon lightning",
+  
+  ## climacon downpour
+  "heavy_rain": "climacon downpour",
+  "heavy_rain_early": "climacon downpour",
+  "heavy_rain_late": "climacon downpour",
+  "lots_of_rain": "climacon downpour",
+  "tons_of_rain": "climacon downpour",
+  "flash_floods": "climacon downpour",
+  "tropical_storm": "climacon downpour",
+  "hurricane": "climacon downpour",
+  "flood": "climacon downpour",
+  
+  
+  # Snow
+  ## climacon snow
+  "snow": "climacon snow",
+  "moderate_snow": "climacon snow",
+  "snow_early": "climacon snow",
+  "snow_late": "climacon snow",
+  "heavy_snow": "climacon snow",
+  "heavy_snow_early": "climacon snow",
+  "heavy_snow_late": "climacon snow",
+  "snowstorm": "climacon snow",
+  "blizzard": "climacon snow",
+  
+  ## climacon snow sun
+  "scattered_flurries": "climacon snow sun",
+  "snow_flurries": "climacon snow sun",
+  "light_snow_showers": "climacon snow sun",
+  "snow_showers": "climacon snow sun",
+  "light_snow": "climacon snow sun",
+  "flurries_early": "climacon snow sun",
+  "snow_showers_early": "climacon snow sun",
+  "light_snow_early": "climacon snow sun",
+  "flurries_late": "climacon snow sun",
+  "snow_showers_late": "climacon snow sun",
+  "light_snow_late": "climacon snow sun",
+  
+  
+  # Haze
+  ## climacon haze sun
+  "hazy_sunshine": "climacon haze sun",
+  "haze": "climacon haze sun",
+  "smoke": "climacon haze sun",
+  "low_level_haze": "climacon haze sun",
+  
+  ## climacon haze moon
+  "night_haze": "climacon haze moon",
+  "night_smoke": "climacon haze moon",
+  "night_low_level_haze": "climacon haze moon",
+  
+  
+  # Fog
+  ## climacon fog
+  "early_fog_followed_by_sunny_skies": "climacon fog",
+  "early_fog": "climacon fog",
+  "light_fog": "climacon fog",
+  "fog": "climacon fog",
+  "dense_fog": "climacon fog",
+  "ice_fog": "climacon fog",
+  
+  
+  # Sleet
+  ## climacon sleet
+  "hail": "climacon sleet",
+  "sleet": "climacon sleet",
+  "light_mixture_of_precip": "climacon sleet",
+  "icy_mix": "climacon sleet",
+  "mixture_of_precip": "climacon sleet",
+  "heavy_mixture_of_precip": "climacon sleet",
+  "snow_changing_to_rain": "climacon sleet",
+  "snow_changing_to_an_icy_mix": "climacon sleet",
+  "an_icy_mix_changing_to_snow": "climacon sleet",
+  "an_icy_mix_changing_to_rain": "climacon sleet",
+  "rain_changing_to_snow": "climacon sleet",
+  "rain_changing_to_an_icy_mix": "climacon sleet",
+  "light_icy_mix_early": "climacon sleet",
+  "icy_mix_early": "climacon sleet",
+  "light_icy_mix_late": "climacon sleet",
+  "icy_mix_late": "climacon sleet",
+  "snow_rain_mix": "climacon sleet",
+  "light_freezing_rain": "climacon sleet",
+  "freezing_rain": "climacon sleet",
+  
+  
+  # Others
+  ## climacon tornado
+  "tornado": "climacon tornado",
+  
+  ## climacon haze
+  "sandstorm": "climacon haze",
+  "duststorm": "climacon haze",
+  
+  ## climacon cloud refresh
+  "cw_no_report_icon": "climacon cloud refresh",
+}
